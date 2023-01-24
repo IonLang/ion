45d31081f497a55dda982f7792a9658f7884a097
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2023-01-25 00:09:43 +0100

    start writing StringLexer

--- src/lexer/StringLexer.cpp
+++ src/lexer/StringLexer.cpp
@@ -5,4 +5,20 @@ using namespace Lexing;
 StringLexer::StringLexer(uint32_t& line, uint32_t& linePos, std::list<Token>& tokens) : Lexer(line, linePos, tokens) {}
 
 void StringLexer::tokenize(std::istream& input) {
+    char c;
+    while (input.good()) {
+        input.get(c);
+        switch (c) {
+            case '"':
+                tokens.push_back(Token(line, linePos, TokenType::doubleQuote));
+                return;
+            case '\\':
+                break;
+            case '$':
+                break;
+            default:
+                buffer += c;
+                break;
+        }
+    }
 }
